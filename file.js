8695e71491158a7f6b321f80da59e446e0ce083d
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-08 14:16:03 +0100

    Secondo metodo senza varibili (Commentato)

--- file.js
+++ file.js
@@ -1,3 +1,4 @@
+// Metodo con variabili
 let nome = prompt("Inserisci il tuo nome");
 let cognome = prompt("Inserisci il tuo cognome");
 let colorePreferito = prompt("Inserisci il tuo colore preferito");
@@ -6,3 +7,10 @@ let eta = prompt("Inserisci la tua età")
 let password = nome + cognome + colorePreferito + eta;
 
 document.getElementById('container_password').innerHTML = password;
+// Posso sommare gli input direttamente senza utilizzo di un'altra varibile
+// document.getElementById('container_password').innerHTML = nome + cognome + colorePreferito + eta;
+
+
+// Metodo senza Variabili
+// document.getElementById('container_password').innerHTML = prompt("Inserisci il tuo nome") + prompt("Inserisci il tuo cognome") + prompt("Inserisci il tuo colore preferito") + prompt("Inserisci la tua età");
+
